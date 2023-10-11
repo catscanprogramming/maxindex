0fda996a70bb19641ec04f92f57c627ffdd07675
catscanprogramming <129110482+catscanprogramming@users.noreply.github.com>
2023-10-11 16:40:44 -0400

    Create maxindex.py

---
+++ maxindex.py
@@ -0,0 +1,17 @@
+def maxindex(data_str: dict|list|tuple)->int:
+    
+    for i in data_str:
+        
+        index = data_str.index(i)
+        
+        maximum = 0
+        
+        if index > maximum:
+            
+            maximum = index
+        
+        else:
+            
+            continue
+        
+        return maximum
